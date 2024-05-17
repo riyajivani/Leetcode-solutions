ffb238fe664ff545dd271347a1e00a857aee6e6d
Riya Jivani <96529641+riyajivani@users.noreply.github.com>
2024-05-17 21:17:13 +0530

    Time: 0 ms (100.00%), Space: 43.9 MB (87.24%) - LeetHub

---
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+
+        if (root == null) {
+            return null;
+        }
+
+        root.left = removeLeafNodes(root.left, target);
+
+        root.right = removeLeafNodes(root.right, target);
+
+        // 3. Check if the current node is a leaf node and its value equals target
+        if (root.left == null && root.right == null && root.val == target) {
+        // Delete the node by returning null to the parent, effectively disconnecting it
+            return null;
+        }
+
+        return root;
+    }
+}
