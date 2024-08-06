ec46d5eff1eab8a64a7bff17f6ab38eb957ebfa4
Riya Jivani <96529641+riyajivani@users.noreply.github.com>
2024-08-06 21:21:55 +0530

    Time: 1 ms (76.40%), Space: 44.7 MB (38.73%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -3,7 +3,7 @@ class Solution {
         
         if (nums.length == 0) return 0;
         
-        int count = 1;  // The first element is always considered unique
+        int count = 1;  
         
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] != nums[count - 1]) {
