0caf813d21149c19c2133ff0dd624568be00ea19
Riya Jivani <96529641+riyajivani@users.noreply.github.com>
2025-05-26 20:54:16 +0530

    Time: 3 ms (41.23%), Space: 41.9 MB (48.06%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            if (!stack.isEmpty()) {
+                char last = stack.peek();
+                if (isPair(last, cur)) {
+                    stack.pop();
+                    continue;
+                }
+            }
+            stack.push(cur);
+        }
+
+        return stack.isEmpty(); 
+    }
+
+    private boolean isPair(char last, char cur) {
+        return (last == '(' && cur == ')') ||
+               (last == '{' && cur == '}') ||
+               (last == '[' && cur == ']');
+    }    
+}
